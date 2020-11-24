84dbf48044cdda8a9aa41c712318cb7696227313
Jake Metherell <jake@fluffylogic.com>
2020-11-24 17:43:17 +0000

    CM-244 - update the hostname for local LDAP server in config.properties to allow for multiple managed servers

--- cic-app/container-scripts/setConfigAndStartNM.sh
+++ cic-app/container-scripts/setConfigAndStartNM.sh
@@ -5,6 +5,9 @@ DOMAIN_HOME="${ORACLE_HOME}/${DOMAIN_NAME}"
 # Update the CIC app config.properties to set the password for ldap access
 sed -i "s/@cic-admin-ldap-password@/${LDAP_CREDENTIAL}/g" ${DOMAIN_HOME}/config.properties
 
+# Update the CIC app config.properties to set the host
+sed -i "s/@ldap-host@/${HOSTNAME}/g" ${DOMAIN_HOME}/config.properties
+
 
 ${ORACLE_HOME}/container-scripts/startNodeManager.sh
 
